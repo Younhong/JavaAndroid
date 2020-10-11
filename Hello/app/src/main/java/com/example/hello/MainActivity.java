c28061c47aca8a32e29465578e31fd27f2edad50
younhong <younhong@kakao.com>
2020-10-11 15:00:10 +0900

    upload

---
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v) {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-5796-0874"));
+        startActivity(myIntent);
+    }
+}
