48cf95c24d05429726e431c2e2f1c0f34c0d911e
younhong <younhong@kakao.com>
2020-11-01 14:12:21 +0900

    update

---
+++ SamplePush/app/src/main/java/org/younhong/samplepush/MainActivity.java
@@ -0,0 +1,14 @@
+package org.younhong.samplepush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
